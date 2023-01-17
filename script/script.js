31f94f039bb13dcda205bc863ce386f238570129
prabakaran <mspk028@gmail.com>
2023-01-17 13:41:53 +0530

    new function added

--- script/script.js
+++ script/script.js
@@ -21,58 +21,66 @@ function getUser() {
     .then((resp) => {
       if (!resp.ok) {
         hiddenEl.style.visibility = "hidden";
+        inputEl.value = "";
         alert("User Not Found...SORRY!!!");
+        throw new Error("No Profile found !!!");
       }
       return resp.json();
     })
     .then((data) => {
-      // profile pic
-      profileEl.innerHTML = `<div class="user-img" id="profile">
-      <img src="${data.avatar_url}" class='img' alt="profile-pic" />
-    </div>`;
-
-      // name
-      if (data.name === null) {
-        gitNameEl.innerHTML = `<h3 class="name">
-      <i class="fa-brands fa-github name-icon"></i>
-    </h3>`;
-      } else {
-        gitNameEl.innerHTML = `<h3 class="name">
-      <i class="fa-brands fa-github name-icon"></i> ${data.name}
-    </h3>`;
-      }
-
-      // bio
-      if (data.bio === null) {
-        bioEl.innerText = "-";
-      } else {
-        bioEl.innerText = `${data.bio}`;
-      }
-
-      // followers/following
-      followersEl.innerHTML = `<span>Followers</span> ${data.followers}`;
-      followingEl.innerHTML = `<span>Following</span> ${data.following}`;
-
-      // repo & gist
-      repoEl.innerHTML = `<span>Repositories</span> ${data.public_repos}`;
-      gistEl.innerHTML = `<span>Public Gists</span> ${data.public_gists}`;
-
-      // location
-      if (data.location === null) {
-        locationEl.innerHTML = `<i class="fa-solid fa-location-dot"></i>`;
-      } else {
-        locationEl.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${data.location}`;
-      }
+      displayResults(data);
     });
+}
+
+function displayResults(data) {
+  // profile pic
+  profileEl.innerHTML = `<div class="user-img" id="profile">
+    <img src="${data.avatar_url}" class='img' alt="profile-pic" />
+  </div>`;
+
+  // name
+  if (data.name === null) {
+    gitNameEl.innerHTML = `<h3 class="name">
+    <i class="fa-brands fa-github name-icon"></i>
+  </h3>`;
+  } else {
+    gitNameEl.innerHTML = `<h3 class="name">
+    <i class="fa-brands fa-github name-icon"></i> ${data.name}
+  </h3>`;
+  }
+
+  // bio
+  if (data.bio === null) {
+    bioEl.innerText = "-";
+  } else {
+    bioEl.innerText = `${data.bio}`;
+  }
+
+  // followers/following
+  followersEl.innerHTML = `<span>Followers</span> ${data.followers}`;
+  followingEl.innerHTML = `<span>Following</span> ${data.following}`;
+
+  // repo & gist
+  repoEl.innerHTML = `<span>Repositories</span> ${data.public_repos}`;
+  gistEl.innerHTML = `<span>Public Gists</span> ${data.public_gists}`;
+
+  // location
+  if (data.location === null) {
+    locationEl.innerHTML = `<i class="fa-solid fa-location-dot"></i>`;
+  } else {
+    locationEl.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${data.location}`;
+  }
+
+  hiddenEl.style.visibility = "visible";
 }
 
 // event listneres
 btnsearch.addEventListener("click", () => {
   if (inputEl.value === "") {
     alert("Please Enter A Valid GitHub Name...");
-    hiddenEl.style.visibility = "hidden";
+    // hiddenEl.style.visibility = "hidden";
   } else {
-    hiddenEl.style.visibility = "visible";
+    // hiddenEl.style.visibility = "visible";
     getUser();
   }
 });
@@ -82,9 +90,8 @@ inputEl.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     if (inputEl.value === "") {
       alert("Please Enter A Valid GitHub Name...");
-      hiddenEl.style.visibility = "hidden";
+      // hiddenEl.style.visibility = "hidden";
     } else {
-      hiddenEl.style.visibility = "visible";
       getUser();
     }
   }
